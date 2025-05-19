0bf2d3ee15695f5dc9a1afe1a42400742f41385a
0xshae <shagun.prasad28@gmail.com>
2025-05-19 23:14:53 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std:: {env, process};
+use tinygrep::Config;
+
+fn main(){
+    let args: Vec<String> = env::args().collect();
+    
+    let config = Config::build(&args).unwrap_or_else(|err|{
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = tinygrep::run(config){
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
